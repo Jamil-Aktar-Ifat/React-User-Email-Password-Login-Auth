5eab0307d784d5d742851c3e8fe84692901fa7d4
ifat <i>
2024-08-23 16:40:05 +0100

    validation added

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,19 +1,34 @@
+import { useState } from "react";
 import auth from "../firebase/firebase.config";
-import {createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 const Register = () => {
+  const [registerError, setRegisterError] = useState("");
+  const [registrationSuccess, setRegistrationSuccess] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
     console.log(email, password);
+    //clear error
+    setRegisterError("");
+    setRegistrationSuccess("");
+    // password validation
+    if (password.length < 6) {
+      setRegisterError("Password should be at least 6 characters or longer");
+      return;
+    }
+
     // create user
     createUserWithEmailAndPassword(auth, email, password)
       .then((result) => {
         console.log(result);
+        setRegistrationSuccess("User Created Successfully");
       })
       .catch((error) => {
         console.log(error);
+        setRegisterError(error.message);
       });
   };
   return (
@@ -26,6 +41,7 @@ const Register = () => {
           name="email"
           id="email"
           placeholder="Email Address"
+          required
         />
         <br />
         <input
@@ -34,6 +50,7 @@ const Register = () => {
           name="password"
           id="password"
           placeholder="Password"
+          required
         />
         <br />
         <input
@@ -42,6 +59,10 @@ const Register = () => {
           className="btn btn-secondary w-3/4  rounded-none"
         />
       </form>
+      {registerError && <p className="text-red-700 mt-5">{registerError}</p>}
+      {registrationSuccess && (
+        <p className="text-green-500 mt-5">{registrationSuccess}</p>
+      )}
     </div>
   );
 };
